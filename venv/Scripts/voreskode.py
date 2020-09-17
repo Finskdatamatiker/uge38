00396a9401a0adb4049ddb8286a0b590da7e85e8
Nobralyra <signees96@gmail.com>
2020-09-17 15:36:35 +0200

    Changed the requirements and code

--- venv/Scripts/voreskode.py
+++ venv/Scripts/voreskode.py
@@ -4,3 +4,7 @@ forside = requests.get('http://www.kea.dk',allow_redirects = True)
 result = forside.text
 print(result)
 
+request_from_kea = requests.get('https://kea.dk/images/resources/logo-main-black-single.png', allow_redirects = True)
+
+with open('kea_image.png', 'wb') as picture:
+    picture.write(request_from_kea.content)
